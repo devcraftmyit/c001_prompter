7f6ce59654ab6e8d85417cf785c6432d97013701
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-10-16 09:40:51 +0100

    351: Clearing other Terminals

--- lib/src/terminal.dart
+++ lib/src/terminal.dart
@@ -4,11 +4,12 @@ class Terminal {
   void printPrompt(String prompt) {
     stdout.writeln(prompt);
   }
-  void clearScreen(){
-    if (Platform.isWindows) {
-stdout.write('\x1B[2j\x1B[0f');
-    } else { 
 
+  void clearScreen() {
+    if (Platform.isWindows) {
+      stdout.write('\x1B[2J\x1B[0f');
+    } else {
+      stdout.write('\x1B[2J\x1B[3J\x1B[H');
     }
   }
 }
