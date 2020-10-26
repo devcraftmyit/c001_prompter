9e2effb348c3c977213d53db8afc7f6a20d40f1e
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-10-26 09:23:38 +0000

    371: Error Handling

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -8,8 +8,11 @@ class Prompter {
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
     final input = _terminal.collectInput();
-
-    return options[int.parse(input)].label;
+    try {
+      return options[int.parse(input)].label;
+    } catch (err) {
+      return ask(prompt, options);
+    }
   }
 
   // retrun options[int.parse(input)];
