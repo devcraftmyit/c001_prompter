f6c15bf8fc7755e0c185db5133add367372db706
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-10-28 08:31:30 +0000

    374: Code Similarities

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -5,12 +5,15 @@ final _terminal = const Terminal();
 
 class Prompter {
   bool askBinary(String prompt) {
+    _terminal.clearScreen();
     _terminal.printPrompt('$prompt Y/N');
+    _terminal.printOptions([]);
     final input = _terminal.collectInput();
     return input.contains('y');
   }
 
   askMultiple(String prompt, List<Options> options) {
+    _terminal.clearScreen();
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
     final input = _terminal.collectInput();
