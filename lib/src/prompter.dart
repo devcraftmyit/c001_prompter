461936cf18ea2976e214a83a1ef534c1c35c26ab
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-10-29 07:30:45 +0000

    375: Refactor for Code Reuse

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -5,23 +5,24 @@ final _terminal = const Terminal();
 
 class Prompter {
   bool askBinary(String prompt) {
-    _terminal.clearScreen();
-    _terminal.printPrompt('$prompt Y/N');
-    _terminal.printOptions([]);
-    final input = _terminal.collectInput();
+    final input = ask('$prompt Y/N', []);
     return input.contains('y');
   }
 
   askMultiple(String prompt, List<Options> options) {
-    _terminal.clearScreen();
-    _terminal.printPrompt(prompt);
-    _terminal.printOptions(options);
-    final input = _terminal.collectInput();
+    final input = ask(prompt, options);
     try {
       return options[int.parse(input)].label;
     } catch (err) {
       return askMultiple(prompt, options);
     }
+  }
+
+  String ask(String prompt, List<Options> options) {
+    _terminal.clearScreen();
+    _terminal.printPrompt(prompt);
+    _terminal.printOptions(options);
+    return _terminal.collectInput();
   }
 
   // retrun options[int.parse(input)];
